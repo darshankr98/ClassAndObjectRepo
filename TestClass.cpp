cd54341e47d0d259f941b3e3809f4d40e3a953f3
Darshan <darshikr98@gmail.com>
2024-07-03 11:47:19 +0530

    comm

---
+++ TestClass.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+class Simple{
+    public :
+    int a,b;
+    public:
+    void getdata()
+    {
+        cout<<"Enter Two Numbers";
+        cin>>a>>b;
+}
+    void printdata()
+    {
+    cout<<a<<endl<<b<<endl;
+}
+};
+/*void Simple :: getdata(void)
+{
+        cout<<"Enter Two Numbers";
+        cin>>a>>b;
+}
+void Simple :: printdata(void)
+{
+    cout<<a<<endl<<b;
+}*/
+
+int main()
+{
+    Simple s;
+    s.getdata();
+    s.printdata();
+    int x = s.a;
+    cout<<x<<endl;
+    return 0;
+}
