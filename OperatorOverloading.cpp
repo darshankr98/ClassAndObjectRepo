0cc7b9f1fb3ce0d9216a3ff48bd8ebdc4d66d105
Darshan <darshikr98@gmail.com>
2024-07-09 23:42:53 +0530

    Operator Overloading exercises

--- OperatorOverloading.cpp
+++ OperatorOverloading.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+/* #include<iostream>
 using namespace std;
 class Complex{
     float x,y;
@@ -14,9 +14,9 @@ class Complex{
         cout<<x<<" + "<<y<<"\n";
     }
 };
-/*Complex Complex :: operator+(Complex c){
-    return Complex((x+c.x),(y+c.y));
-}*/
+//Complex Complex :: operator+(Complex c){
+  //  return Complex((x+c.x),(y+c.y));
+//}
 Complex operator +(Complex a, Complex b){
     return Complex(a.x+b.x,a.y+b.y);
 }
@@ -31,4 +31,31 @@ int main()
     C1.Display();
     C2.Display();
     C3.Display();
+} */
+
+
+
+#include<iostream>
+using namespace std;
+class arr{
+    int a[5];
+    public:
+    arr(int *x){
+        for(int i = 0;i<5;i++){
+            a[i] = x[i];
+        }
+    }
+    int operator [](int k){
+        cout<<"Overloading";
+        return (a[k]);            
+        }
+};
+
+
+int main(){
+    int x[5] = {1,2,3,4,5};
+    arr A(x);
+    for(int i =0;i<5;i++){
+        cout<<x[i]<<"\t";
+    }
 }
